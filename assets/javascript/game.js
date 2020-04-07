f71ed00d214697d289850cf16d9e3c630778315a
bliff182 <michael.bither@gmail.com>
2020-04-07 13:27:02 -0400

    total redesign, wireframe done

---
+++ assets/javascript/game.js
@@ -0,0 +1,60 @@
+// $(document).ready(function() {
+
+// // GLOBAL VARIABLES AND ARRAYS
+// // =======================================================
+
+//     // character variables
+//     var characters = {
+//         luke: {
+//             health: 100,
+//             attack: 10,
+//             counter: 5
+//         },
+//         obiWan : {
+//             health: 100,
+//             attack: 10,
+//             counter: 5
+//         },
+//         vader: {
+//             health: 150,
+//             attack: 6,
+//             counter: 20
+//         },
+//         darthMaul: {
+//             health: 180,
+//             attack: 4,
+//             counter: 25
+//         }
+//     };
+//     var characterChoice;
+//     var attacker;
+//     var enemyChoice;
+//     var defender;
+
+//     // Game counters
+//     var wins = 0;
+//     var losses = 0;
+
+//     // Booleans
+//     var isCharacterChosen = false;
+//     var isEnemyChosen = false;
+
+// // FUNCTIONS
+// // =======================================================
+
+//     // Choose character
+//     function chooseCharacter () {
+//         isCharacterChosen = true;
+//         $(".character-div").attr("health", this.health).attr("attack", this.attack).attr("counter", this.counter);
+//     }
+
+// // MAIN PROCESS
+// // =======================================================
+
+//     $(".character-div").on("click", function(){
+//         if (!isCharacterChosen) {
+//             $(".character-div").attr()
+//         }
+//     })
+
+// });
